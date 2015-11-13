df0711a9b75a8c35e8729e05c37abc9ebd6ad618
stephanie miranda <stephanie.miranda.valdez@gmail.com>
2015-11-13 09:10:17 -0600

    nothing changed

--- javascripts/get-books.js
+++ javascripts/get-books.js
@@ -4,7 +4,7 @@ define(function(require) {
   return {
     load: function(fn) {
       // This XHR should be in its own require module, not here
-      $.ajax("https://nss-book-store.firebaseio.com/booktypes.json").done(function(types) {
+      $.ajax("https://nss-book-store.firebaseio.com/booktypes.json").done(function() {
 
         // This XHR does belong here
         $.ajax("https://nss-book-store.firebaseio.com/books.json").done(function(books) {
@@ -17,17 +17,19 @@ define(function(require) {
             version of JavaScript syntax). They are called fat arrows.
             Check out the docs at http://es6-features.org/#ExpressionBodies
           */
-          types = Object.keys( types ).map(key => types[ key ]);
+          // types = Object.keys( types ).map(key => types[ key ]);
           books = Object.keys( books ).map(key => books[ key ]);
 
           /*
             I'm using the lodash `find()` method here.
               https://lodash.com/docs#find
+              _.find(collection, [predicate=_.identity], [thisArg])
            */
           var books = books.map(book => {
             book.type = _.find(types, { id:book.booktype }).label;
             return book;
           });
+            console.log("books", books);
 
           // Still relying on a callback? That's so 2014...
           fn(books);
